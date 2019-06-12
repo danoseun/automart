33b987453a84bf0d36d779cec1cfbecdc5fa4205
danoseun <ayodejisomefun@yahoo.com>
2019-06-12 23:45:21 +0100

    Implement functionality to update order price

--- server/routes/order.js
+++ server/routes/order.js
@@ -3,10 +3,11 @@ import { OrderController } from '../controllers';
 import { OrderValidator } from '../validations';
 import { verifyToken } from '../middlewares/auth';
 
-const { postOrder } = OrderController;
+const { postOrder, editOrderPrice } = OrderController;
 const { postOrderChecker } = OrderValidator;
 
 export const orderRouter = express.Router();
 
 
 orderRouter.post('/order', verifyToken, postOrderChecker, postOrder);
+orderRouter.patch('/order/:orderId/price', verifyToken, editOrderPrice);
