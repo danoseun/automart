6eac0232d25ed6ecf8ca9ce6cb48b92ade502423
danoseun <ayodejisomefun@yahoo.com>
2019-07-16 15:43:20 +0100

    fix another sql insert error

--- server/config/sql.js
+++ server/config/sql.js
@@ -24,10 +24,10 @@ export const bodyTypeQuery = 'SELECT * FROM cars WHERE body_type = $1';
 export const statusStateQuery = 'SELECT * FROM cars WHERE status = $1 AND state = $2';
 
 /** Order Query */
-export const postOrderQuery = 'INSERT INTO orders (buyerid, car_id, amount) VALUES ($1, $2, $3) returning *';
+export const postOrderQuery = 'INSERT INTO orders (buyer_id, car_id, amount) VALUES ($1, $2, $3) returning *';
 export const findOrderQuery = 'SELECT * FROM orders WHERE id = $1 AND buyer_id = $2';
 export const updateOrderQuery = 'UPDATE orders SET amount = $1 WHERE id = $2 and buyer_id = $3 returning *';
 export const allUserOrdersQuery = 'SELECT * FROM orders WHERE buyer_id = $1';
 
 /** Flag Query */
-export const createFlagQuery = 'INSERT INTO flags (userid, carid, reason, description) VALUES ($1, $2, $3, $4) returning *';
+export const createFlagQuery = 'INSERT INTO flags (userid, car_id, reason, description) VALUES ($1, $2, $3, $4) returning *';
